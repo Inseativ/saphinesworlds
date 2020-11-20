1bd470c2579f8be9538b8979d5143b9dae30b887
Glitch (verbose-stormy-deltadromeus) <none>
2020-11-20 08:49:50 +0000

    Import of GweepCreative/cekilis

--- events/message.js
+++ events/message.js
@@ -1,9 +1,21 @@
 const ayarlar = require('../ayarlar.json');
-module.exports = message => {
+const db = require('quick.db');
+let talkedRecently = new Set();
+module.exports = async message => {
+if(message.author.bot) return
+
+  if (talkedRecently.has(message.author.id)) {
+    return;
+  }
+  talkedRecently.add(message.author.id);
+	setTimeout(() => {
+    talkedRecently.delete(message.author.id);
+  }, 2500);
   let client = message.client;
+  let prefix = await db.fetch(`prefix_${message.guild.id}`) || ayarlar.prefix
   if (message.author.bot) return;
-  if (!message.content.startsWith(ayarlar.prefix)) return;
-  let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
+  if (!message.content.startsWith(prefix)) return;
+  let command = message.content.split(' ')[0].slice(prefix.length);
   let params = message.content.split(' ').slice(1);
   let perms = client.elevation(message);
   let cmd;
@@ -13,8 +25,13 @@ module.exports = message => {
     cmd = client.commands.get(client.aliases.get(command));
   }
   if (cmd) {
+  let bakım = await db.fetch('bakım');
+  if(message.author.id !== ayarlar.sahip){
+  if(bakım){
+  return message.channel.send(`**<a:hype:742101366129819680> Sizlere En İyi Hizmeti Verebilmek İçin Bakımdayız.\n<:soru:742101322869899289> Bakım Sebebi: \`${bakım}\`\n<a:hype:742101366129819680> Lütfen Daha Sonra Tekrar Deneyin.**`)
+     }
+    }
     if (perms < cmd.conf.permLevel) return;
     cmd.run(client, message, params, perms);
   }
-
 };
--- events/ready.js
+++ events/ready.js
@@ -1,33 +1,19 @@
-const chalk = require('chalk');
-const moment = require('moment');
-const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
-
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
 var prefix = ayarlar.prefix;
 
-module.exports = client => {
-var oyun = [
-        "🔥",
-        "✨",
-        "🔥",
-        "🔥",
-        "⭐️",
-        "👿",
-       //DBL EKLENCEKSENİZ 5000 EN İYİ DEĞİŞİM SÜRESİDİR! EKLEMEK İSTEMİYORSANIZ İDEAL 2500
+module.exports = async client => {
+  var oyun = [prefix +"yardım 🔥 " + prefix +"davet"];
 
-    ];
+  setInterval(async () => {
+    var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
 
-    setInterval(function() {
-
-        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
-
-        client.user.setActivity(oyun[random], "https://www.youtube.com/channel/UC5mxQbR-4er2giWDTq9SgQg?view_as=subscriber" );
-      }, 2 * 5000); //DEĞİŞME SÜRESİ
-
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("online");
-  client.user.setActivity(`${prefix}yardım + ${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
-  //console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  //console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+    client.user.setActivity(oyun[random], { type: "WATCHING" });
+  }, 13000);
+  client.user.setStatus("dnd");
+console.log("Friday Hazır Gweep")
+  /*client.user.setActivity("a!yardım | a!davet | a!prefix ☣", { type: "WATCHING" });*/
 };
+
